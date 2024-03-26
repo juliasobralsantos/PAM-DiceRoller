eacabe8075ab242c3347d72f4b2469faa49caa7e
juliasobralsantos <juliasobralsantos@hotmail.com>
2024-03-25 22:34:37 -0300

    prova

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public virtual int Next (int minValue, int maxValue)
+        {
+            return 0;
+        }
+
+        private void DiceRoller(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
